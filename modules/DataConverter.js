994d98534e049d87c4ab47af0f133c92d0612147
Gibson <anderhan@student.chalmers.se>
2019-02-23 20:28:02 +0100

    Add search functions and data conversion functions

---
+++ modules/DataConverter.js
@@ -0,0 +1,81 @@
+/**
+ * Converts the response to the country code
+ * Example of the converted data from country name 'France': 'FR'
+ * @param {Object} searchData - response from a fetch
+ * @param {string} countryName - name of country searched
+ * @returns {string} a country code of the results
+ */
+
+export function convertToCountryCode(searchData, countryName){
+    const data = searchData.geonames;
+
+    // Valid data check
+    if( data === undefined || 
+        data.length == 0 ||
+        data[0].countryName != countryName){
+
+        return null;
+
+    }else {
+       return data[0].countryCode;
+    }
+   
+}
+
+/**
+ * Converts the response to an array where each even element is a city name
+ * and each odd is the population of the previous element
+ * Example of converted data from counrty code 'FR': ['Paris', 2133451, 'Marseille', 861321, 'Lyon', 513432 ...]
+ * @param {Object} searchData - response from a fetch
+ * @param {string} countryCode - the country code searched for
+ * @returns {Array} the converted data
+ */
+export function convertToName(searchData, countryCode){
+    const data = searchData.geonames;
+
+        // Valid data check
+         if( data === undefined || 
+             data.length == 0) {
+ 
+            return null;
+
+         }else {
+             //Separates each element into name and population
+             var dataArray = [];
+             data.forEach(element => {
+                 if(element.countryCode = countryCode){
+                    dataArray.push(element.name);
+                    dataArray.push(element.population);
+                    }
+             });
+            return dataArray;
+        }
+}
+
+/**
+ * Converts the response to data in the form of: [cityName, cityPopulation]
+ * Example data that is returned from city name 'Berlin': ['Berlin', 34265354]
+ * @param {Object} searchData - response from a fetch
+ * @param {string} cityName - city search for
+ * @returns {Array} the converted data
+ */
+
+export function convertToCityAndPopulation(searchData, cityName){
+    const data = searchData.geonames;
+
+    // Valid data check
+    if( data === undefined || 
+        data.length == 0 ||
+        data[0].name != cityName ||
+        data[0].population == 0){
+
+        return null;
+
+    }else {
+       return [data[0].name, data[0].population];
+    }
+
+}
+
+
+
--- modules/GeoSearch.js
+++ modules/GeoSearch.js
@@ -1,29 +1,63 @@
+import {
+    convertToCityAndPopulation, 
+    convertToCountryCode, 
+    convertToName
+} from './DataConverter'
+/**
+ * Function to convert the data recieved from fetch
+ * @callback dataConvertFunction
+ * @param {Object} response - the json response of a fetch call
+ * @param {string} searchTerm - the term searched for
+ */
 
-export async function searchCity(cityName){
-    URL = 'http://api.geonames.org/searchJSON?name=' + cityName + '&featureClass=P&maxRows=5&username=weknowit';
-    
+ /**
+ * Makes and handles a fetch call od the URL then converts it using the dataConvertFunc parameter
+ * @param {string} URL - the URL to be fetched from
+ * @param {string} searchTerm - the term searched for 
+ * @param {dataConvertFunction} dataConvertFunc - function to convert the data
+ */
+async function search(URL, searchTerm, dataConvertFunc){
     try{
         let response = await fetch(URL);
         if(response.status > 400){
           return {};
         } else {
-          let search = await response.json();
+          let r = await response.json();
 
-          const data = search.geonames;
-
-          if( data === undefined || 
-              data.length == 0 ||
-              data[0].name != cityName ||
-              data[0].population == 0){
-      
-              return null;
-      
-          }else {
-             return [data[0].name, data[0].population];
-          }
+          return dataConvertFunc(r, searchTerm);
         }
       } catch(e) {
         return console.log(e);
       }
     
+}
+
+/**
+ * Function that search for populated places with the same name as parameter cityName
+ * @param {string} cityName - name of city to be search for
+ * @returns A Promise object with data in the form of an array: [CityName, Population]
+ */
+export async function searchCity(cityName){
+    URL = 'http://api.geonames.org/searchJSON?name=' + cityName + '&featureClass=P&maxRows=5&username=weknowit';
+    return search(URL, cityName, convertToCityAndPopulation);    
+}
+
+/**
+ * Function that searches for a country and retrieves its country code if found
+ * @param {string} countryName - country to be seached in 
+ * @returns A Promise object with data in the form of a country code: 'FR'
+ */
+export async function searchCountry(countryName){
+    URL = 'http://api.geonames.org/searchJSON?name_equals=' + countryName + '&featureClass=A&orderby=population&maxRows=5&username=weknowit';
+    return search(URL, countryName, parseCountryToCode);
+}
+
+/**
+ * Function that searches for cities in a country with country code of the parameter countryCode
+ * @param {string} countryCode - country code of the country to be searched in
+ * @returns A Promise object with data in the form of an array: [cityName1, population1, CityName2, population2, ... cityNameX, populationX]
+ */
+export async function searchCityInCountry(countryCode){
+    URL = 'http://api.geonames.org/searchJSON?country=' + countryCode + '&featureClass=P&orderby=population&maxRows=5&username=weknowit';
+    return search(URL, countryCode, parseCityToName);
 }
