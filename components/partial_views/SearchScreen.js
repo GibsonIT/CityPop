f25ba2f5390b5f934b726979ebcbb17dc10a5a9b
Gibson <anderhan@student.chalmers.se>
2019-02-23 17:03:02 +0100

    Add a component for a general search screen
    Used for the city search and country search screens

---
+++ components/partial_views/SearchScreen.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import {TextInput} from 'react-native';
+import PropTypes from 'prop-types'
+import Button from '../../components/Button'
+import TitleContentDivider from './TitleContentDivider';
+
+/**
+ * Meant to be used over the whole screen. Presents a screen with a title, a search bar and a search button.
+ * Will return the text to the callback function in the input field when search button is pressed.
+ */
+
+class SearchScreen extends Component{
+
+    /** initializes the the state text and binds the setText function to this component */
+    constructor(props){
+        super(props);
+        this.state = {text: ''}
+        this.setText = this.setText.bind(this)
+    }
+
+    /** 
+     * Stores the newText parameter in the state, used for the text input
+     * @param {string} newtext - text to be stored
+     */
+    setText(newtext){
+        this.setState({ text: newtext });
+    }
+
+    /** Renders the component */
+    render(){
+        let {title,
+            placeholder,
+            onPress} = this.props;
+
+        return(
+                <TitleContentDivider title = {title}>
+                        
+                    <TextInput placeholder = {placeholder} onChangeText = {this.setText}/>
+
+                    <Button title = "Search" onPress = {() => onPress(this.state.text)}/>
+
+                    {this.props.children}
+
+                </TitleContentDivider> 
+        )
+    }
+}
+
+SearchScreen.propTypes = {
+    /** Title of the screen, preferably what to be searched */
+    title: PropTypes.string.isRequired,
+
+    /** Placeholder text of the input field*/
+    placeholder: PropTypes.string,
+
+    /** 
+     * Callback function to be called when the search button is pressed
+     * Need to accept a parameter of type string
+    */
+    onPress: PropTypes.func.isRequired
+}
+
+/** Default values of the props */
+SearchScreen.defaultProps = { placeholder: 'Enter text'}
+
+export default SearchScreen;
